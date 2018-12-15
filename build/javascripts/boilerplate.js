eaa85b2a14645e99b4627c0aec33c32ece1029fa
cheeseymeaty <cheeseymeaty@gmail.com>
2018-12-14 22:43:47 -0500

    Added a settings and keyboard variable, key events, a console description, and some tweaks

--- build/javascripts/boilerplate.js
+++ build/javascripts/boilerplate.js
@@ -1,23 +1,51 @@
-// Declarations
+// < Declarations >
 const canvas = document.querySelector('canvas');
 const C = canvas.getContext('2d');
 
 let { width, height } = canvas;
 
-// Animation
+const settings = {};
+
+const keyboard = {};
+
+// < Animation >
 function animate() {
 	requestAnimationFrame(animate);
 	C.clearRect(0, 0, width, height);
 	C.fillText('boilerplate.js', 15, 15);
 }
 
-// Events
-onresize = () => {
+// < Events >
+onkeydown = E => {
+	// Switch case for key presses
+	switch (E.code) {
+
+	}
+};
+
+onkeyup = E => {
+	// Switch case for key releases
+	switch (E.code) {
+
+	}
+};
+
+onresize = function() {
 	width = canvas.width = innerWidth;
 	height = canvas.height = innerHeight;
 };
 
 onload = () => {
+	console.log(`
+Quote
+(Reference)
+
+
+Description
+
+- Keys and controls
+`);
+
 	document.title = 'boilerplate';
 	onresize();
 	animate();
