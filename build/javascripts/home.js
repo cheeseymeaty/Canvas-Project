0dbe4cba172345190d3af8ed727b01c6f6b78f03
cheeseymeaty <cheeseymeaty@gmail.com>
2018-12-21 14:16:59 -0500

    Added a homepage that can load all canvas projects without restarting the program

---
+++ build/javascripts/home.js
@@ -0,0 +1,17 @@
+const PROJECTS = ['Chaos-Game-Triangle', 'Monges-Theorem', 'Vivianis-Theorem'];
+
+// STOP
+import $ from 'jquery';
+
+const paragraph = 'This is the Home Page. Use this to load a canvas project. You can come back here by going to "localhost:8080/build/". Remember to open the console (F12) to read their descriptions!';
+
+$('<h1>Welcome To Canvas Projects!<h1>').appendTo(document.body);
+$(`<p>${paragraph}<p>`).appendTo(document.body);
+$('<a target="left" href="https://github.com/cheeseymeaty/Canvas-Project">GitHub<a><br><br>').appendTo(document.body);
+$('<p>Projects:<p>').appendTo(document.body);
+
+$('<ol id="list"></ol>').appendTo(document.body);
+
+for (const name of PROJECTS) {
+	$(`<li><a href="http://localhost:8080/build/?project=${name}">${name}</a></li>`).appendTo($('#list'));
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,28 @@
+const fs = require('fs');
+
+// Lists of projects inside ./build/javscripts/
+// / In the format of: { filename: filepath }
+const projects = (() => fs.readdirSync('./build/javascripts')
+.reduce((entry, file) => {
+	entry[file.slice(0, -3)] = `./build/javascripts/${file}`;
+	return entry;
+}, {})
+)();
+
+// Updates home.js
+const exceptions = ['boilerplate', 'home'];
+const data = fs.readFileSync('./build/javascripts/home.js', 'UTF8')
+const replacement = 'const PROJECTS = [' + Object.keys(projects).filter(name => !exceptions.includes(name)).map(name => `'${name}'`).join(', ') + '];\r\n\r\n';
+fs.writeFileSync('./build/javascripts/home.js',
+	replacement +
+	data.slice(data.indexOf('// STOP'))
+);
+
 module.exports = {
 	mode: 'development',
-	entry: `./build/javascripts/${process.env.npm_config_project}.js`,
+	entry: projects,
 	output: {
-		publicPath: '/build/',
-		filename: 'bundle.js'
+		publicPath: '/build/dist/',
+		filename: '[name].js'
 	}
 };
